48396e2691a6f111fc338370b4880a0871b2eb18
Samuel Timbó <samuelmtimbo@gmail.com>
2021-05-17 12:58:09 -0300

    ...

--- index.js
+++ index.js
@@ -100,7 +100,9 @@ app.use(function (req, res, next) {
 });
 // HTTP
 const HTTPServer = http.createServer({}, app);
-HTTPServer.listen(HTTP_PORT);
+HTTPServer.listen(HTTP_PORT, () => {
+    console.log(`HTTP server listening at port ${HTTP_PORT}`);
+});
 // HTTPS
 if (prod) {
     function getSecureContext(domain) {
@@ -128,7 +130,9 @@ if (prod) {
         },
     };
     const HTTPSServer = https.createServer(HTTPSOpt, app);
-    HTTPSServer.listen(HTTPS_PORT);
+    HTTPSServer.listen(HTTPS_PORT, () => {
+        console.log(`HTTPS server listening at port ${HTTPS_PORT}`);
+    });
 }
 const wss = new WebSocket.Server({
     port: 4000,
